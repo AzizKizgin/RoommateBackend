64fd92fd9521f4fbf00f243eae1805ec23011285
Aziz Kızgın <azizkzgn@gmail.com>
2024-03-28 03:10:59 +0300

    feat: Room model updated

--- RoommateBackend/Models/Room.cs
+++ RoommateBackend/Models/Room.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,20 +12,16 @@ namespace RoommateBackend.Models
         public int Price { get; set; }
         public int RoomCount { get; set; }
         public int BathCount { get; set; }
-        public List<byte[]> Images { get; set; } = new List<byte[]>();
-        public Location Location { get; set; }
+        public List<RoomImage> Images { get; set; } = new List<RoomImage>();
         public double Size { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public AppUser Owner { get; set; }
         public string OwnerId { get; set; }
         public List<AppUser> SavedBy { get; set; } = new List<AppUser>();
-    }
-
-    public class Location
-    {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public string Address { get; set; } = string.Empty;
     }
 }
