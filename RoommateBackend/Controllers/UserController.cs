5650a519ec536792cd82f1f1b83b7caf4530b242
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-05 20:54:37 +0300

    feat: UpdateUser ModelState checking

--- RoommateBackend/Controllers/UserController.cs
+++ RoommateBackend/Controllers/UserController.cs
@@ -56,6 +56,12 @@ namespace RoommateBackend.Controllers
                     return BadRequest("Invalid user data.");
                 }
                 
+                var existingUser = await _userManager.FindByEmailAsync(user.Email);
+                if (existingUser != null)
+                {
+                    return BadRequest("User with this email already exists.");
+                }
+
                 var newUser = await _userRepository.CreateUser(user);
                 if (newUser == null)
                 {
@@ -157,6 +163,10 @@ namespace RoommateBackend.Controllers
         {
             try
             {
+                if (ModelState.IsValid == false)
+                {
+                    return BadRequest("Invalid user data.");
+                }
                 var username = User.Identity.Name;
                 if (username == null)
                 {
